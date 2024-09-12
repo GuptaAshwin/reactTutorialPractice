25d864b4b38b794fa46218445789b893d4bc8225
AshwinGuptaRangde <ashwin@rangde.in>
2024-09-12 10:28:52 +0530

    adding contrast color value

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -15,31 +15,32 @@ function Menu() {
     // const { theme, toggleTheme, contrastTheme } = useTheme();
 
     const dispatch = useDispatch();
-    const theme = useSelector(state => state.theme.value); // this step also subscribes to the store, and the component will be re-rendered whenever the selected value (state.theme.value) changes
+    const { value, contrastValue } = useSelector(state => state.theme); // this step also subscribes to the store, and the component will be re-rendered whenever the selected value (state.theme) changes
 
     return (
-        <Navbar collapseOnSelect expand="lg" className={`bg-${theme}`}>
+        <Navbar collapseOnSelect expand="lg" className={`bg-${value}`}>
             <Container>
-                <Navbar.Brand to="/" as={NavLink} target="_blank" className={`text`}>Workshops App</Navbar.Brand>
+                <Navbar.Brand to="/" as={NavLink} target="_blank" className={`text-${contrastValue}`}>Workshops App</Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link to="/" as={NavLink} end className={`text`}>Home</Nav.Link>
-                        <Nav.Link to="/workshops" as={NavLink} end className={`text`}>List of workshops</Nav.Link>
-                        <Nav.Link to="/workshops/add" as={NavLink} className={`text`}>Add a workshop</Nav.Link>
+                        <Nav.Link to="/" as={NavLink} end className={`text-${contrastValue}`}>Home</Nav.Link>
+                        <Nav.Link to="/workshops" as={NavLink} end className={`text-${contrastValue}`}>List of workshops</Nav.Link>
+                        <Nav.Link to="/workshops/add" as={NavLink} className={`text-${contrastValue}`}>Add a workshop</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#" onClick={() => {
                             toggleTheme(); // not necessary
                             dispatch(toggleTheme());
-                        }} className={`text`}>Change theme</Nav.Link>
+                        }} className={`text-${contrastValue}`}>Change theme</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
 }
+
 
 export default Menu;
--- src/slices/theme.js
+++ src/slices/theme.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    value: 'light'
+    value: 'light',
+    contrastValue: 'dark'
 };
 
 const themeSlice = createSlice({
